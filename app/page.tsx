e0c6225018f8c546faf3dff96e884a28a414697d
Greg <Godly@udel.edu>
2023-12-12 17:46:19 -0500

    For Deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
 import Hero from "./Components/Hero"
 import NavBar from "./Components/NavBar"
-import { About } from "./Pages"
 
 export default function Home() {
   return (
